5f5dda39a0ec32e8b0e833b2a77ee66c5c5e1903
AndreaGul <andreagulino99@gmail.com>
2023-11-10 11:28:03 +0100

    feat:aggiunta commenti snak1

--- snak1/js/main.js
+++ snak1/js/main.js
@@ -3,18 +3,25 @@
 // L’utente inserisce due parole in successione, con due prompt.
 // Il software stampa prima la parola più corta, poi la parola più lunga.
 
+//creo due variabili che chiedono all'utente una parola
 const parUtente1 = prompt('inserisci parola');
 const parUtente2 = prompt('inserisci parola');
 
+//paragono la lunchezza delle due parole
+//se la prima e più lunga della seconda
 if (parUtente1.length > parUtente2.length) {
   console.log(
     `la parola dell'utente 1 (${parUtente1}) è più lunga di quella dell'utente 2 (${parUtente2})`
   );
-} else if (parUtente1.length < parUtente2.length) {
+}
+//altrimennti se la prima e più corta della seconda
+else if (parUtente1.length < parUtente2.length) {
   console.log(
     `la parola dell'utente 1 (${parUtente1}) è più corta di quella dell'utente 2 (${parUtente2})`
   );
-} else {
+}
+//alrimenti se sono uguali
+else {
   console.log(
     `le due (${parUtente1}) e (${parUtente2}) parole hanno la stessa lunghezza`
   );
