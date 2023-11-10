d4ccbbda9a6a9afd4ca9d00f191d127b2b8c728e
AndreaGul <andreagulino99@gmail.com>
2023-11-10 11:33:32 +0100

    fix:correzione condizioni rispetto alla traccia

--- snak1/js/main.js
+++ snak1/js/main.js
@@ -9,15 +9,15 @@ const parUtente2 = prompt('inserisci parola');
 
 //paragono la lunchezza delle due parole
 //se la prima e più lunga della seconda
-if (parUtente1.length > parUtente2.length) {
+if (parUtente1.length < parUtente2.length) {
   console.log(
-    `la parola dell'utente 1 (${parUtente1}) è più lunga di quella dell'utente 2 (${parUtente2})`
+    `la parola dell'utente 1 (${parUtente1}) è più corta di quella dell'utente 2 (${parUtente2})`
   );
 }
 //altrimennti se la prima e più corta della seconda
-else if (parUtente1.length < parUtente2.length) {
+else if (parUtente2.length < parUtente1.length) {
   console.log(
-    `la parola dell'utente 1 (${parUtente1}) è più corta di quella dell'utente 2 (${parUtente2})`
+    `la parola dell'utente 2 (${parUtente2}) è più corta di quella dell'utente 1 (${parUtente1})`
   );
 }
 //alrimenti se sono uguali
