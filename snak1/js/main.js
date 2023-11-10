68db6c0485497fa704187798ee73e7b4c5841a27
AndreaGul <andreagulino99@gmail.com>
2023-11-10 11:34:28 +0100

    fix: commenti snak 1

--- snak1/js/main.js
+++ snak1/js/main.js
@@ -8,13 +8,13 @@ const parUtente1 = prompt('inserisci parola');
 const parUtente2 = prompt('inserisci parola');
 
 //paragono la lunchezza delle due parole
-//se la prima e più lunga della seconda
+//se la prima e più corta della seconda
 if (parUtente1.length < parUtente2.length) {
   console.log(
     `la parola dell'utente 1 (${parUtente1}) è più corta di quella dell'utente 2 (${parUtente2})`
   );
 }
-//altrimennti se la prima e più corta della seconda
+//altrimennti se la seconda e più corta della prima
 else if (parUtente2.length < parUtente1.length) {
   console.log(
     `la parola dell'utente 2 (${parUtente2}) è più corta di quella dell'utente 1 (${parUtente1})`
