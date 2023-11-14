a9ab2c944b829079d994f938193fd93c38bcf88c
AndreaGul <andreagulino99@gmail.com>
2023-11-14 10:19:06 +0100

    feat: snak4 con ciclo for

---
+++ jsnack4/js/main.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// Crea un array vuoto
+const num = [];
+
+//chiedi all’utente un numero da inserire nell’array.
+
+// Continua a chiedere i numeri all’utente e a inserirli nell’array fino a quando la somma degli elementi è inferiore di 50.
+
+let numSum = 0;
+
+for (let i = 0; numSum < 50; i++) {
+  num[i] = Number(prompt('inseirsci un numero'));
+  console.log(num[i]);
+
+  numSum += num[i];
+}
+
+console.log(numSum);
+// let numSum = 0;
+// let i = 0;
+// while (numSum < 50) {
+//   num[i] = Number(prompt('inserisci un numero'));
+//   numSum += num;
+//   i++;
+//   console.log(num);
+
+//   console.log(numSum);
+// }
