07afe78505476f435cdd26c994052ee319167979
AndreaGul <andreagulino99@gmail.com>
2023-11-14 10:24:23 +0100

    feat:snack4 con ciclo while

--- jsnack4/js/main.js
+++ jsnack4/js/main.js
@@ -7,23 +7,23 @@ const num = [];
 
 // Continua a chiedere i numeri all’utente e a inserirli nell’array fino a quando la somma degli elementi è inferiore di 50.
 
-let numSum = 0;
+// let numSum = 0;
 
-for (let i = 0; numSum < 50; i++) {
+// for (let i = 0; numSum < 50; i++) {
+//   num[i] = Number(prompt('inseirsci un numero'));
+//   console.log(num[i]);
+
+//   numSum += num[i];
+// }
+// console.log(numSum);
+
+let numSum = 0;
+let i = 0;
+while (numSum < 50) {
   num[i] = Number(prompt('inseirsci un numero'));
   console.log(num[i]);
 
   numSum += num[i];
+  i++;
 }
-
 console.log(numSum);
-// let numSum = 0;
-// let i = 0;
-// while (numSum < 50) {
-//   num[i] = Number(prompt('inserisci un numero'));
-//   numSum += num;
-//   i++;
-//   console.log(num);
-
-//   console.log(numSum);
-// }
